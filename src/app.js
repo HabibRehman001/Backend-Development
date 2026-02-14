b719b355ea23f162537c0d96556515cd9670808d
habibrehman001 <khuwajaabdulrehman046@gmail.com>
2026-02-14 17:39:52 +0500

    More Methods testing

--- src/app.js
+++ src/app.js
@@ -8,19 +8,36 @@ const noteModel = require('./models/note.model')
 // DELETE /notes/:id => Delete a note
 // PATCH /notes/:id => Update a note
 
-app.post("/notes" , async (req,res) => {
+app.post("/notes", async (req, res) => {
     const data = req.body;
     await noteModel.create({
-        title : data.title,
-        description : data.description
+        title: data.title,
+        description: data.description
     })
-    res.status(201).json({message:'Data Saved Successfully'})
+    res.status(201).json({ message: 'Data Saved Successfully' })
 })
-app.get("/notes", async (req,res) => {
-    const notes = await noteModel.find({
-        title:'AbdulRehman'
+app.get("/notes", async (req, res) => {
+    const notes = await noteModel.find();
+    res.status(200).json({ messgage: 'Notes Fetched Successfull!!', notes: notes });
+})
+app.delete("/notes/:id", async (req, res) => {
+    const id = req.params.id;
+    await noteModel.findOneAndDelete({
+        _id: id
     });
-    res.status(200).json({messgage:'Notes Fetched Successfull!!',notes:notes});
+    res.status(200).json({ message: 'Deleted Successfully' })
 })
+app.patch("/notes/:id", async (req, res) => {
+    const id = req.params.id;
+    let description = req.params.description;
+    await noteModel.findOneAndUpdate({
+        _id: id
+    },
+        {
+            description: description
+        }
+    )
+    res.status(200).json({ message: "Description Updated Successfully" });
 
+})
 module.exports = app 
